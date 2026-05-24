bfc8f26fc24e66ee790bed0b60f0010bf1cb2261
nduokikabeverly <nduokikabeverly@gmail.com>
2026-05-24 19:57:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Thank you for contacting Beverly! Your message has been received.");
+  });
+  
